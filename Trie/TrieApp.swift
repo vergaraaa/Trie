7093eb550592432ec94e682dae9daa9ca3ee592d
Edgar Vergara <edgarvm13@hotmail.com>
2024-03-25 15:30:41 +0100

    Initial Commit

---
+++ Trie/TrieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrieApp.swift
+//  Trie
+//
+//  Created by Edgar Ernesto Vergara Montiel on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TrieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
