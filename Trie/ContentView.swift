77478539320351d16f2b7a5533164e3523bc6ae0
Edgar Vergara <edgarvm13@hotmail.com>
2024-04-04 16:51:50 +0200

    feat: add fruit

--- Trie/ContentView.swift
+++ Trie/ContentView.swift
@@ -12,10 +12,12 @@ class ViewModel: ObservableObject {
 }
 
 struct ContentView: View {
-    @State private var wordToAdd = ""
-    @State private var emojiToAdd = ""
     @State private var wordToSearch = ""
     @State private var searchResult: (Bool, Character?) = (false, nil)
+    
+    @State private var wordToAdd = ""
+    @State private var emojiToAdd = ""
+    @State private var showModal: Bool = false
     
     var trie = Trie()
     
@@ -25,44 +27,81 @@ struct ContentView: View {
         trie.insert(word: "Pear", emoji: "🍐")
         trie.insert(word: "Orange", emoji: "🍊")
         trie.insert(word: "Lemon", emoji: "🍋")
-        trie.insert(word: "Banana", emoji: "🍌")
-        trie.insert(word: "Watermelon", emoji: "🍉")
-        trie.insert(word: "Mango", emoji: "🥭")
-        trie.insert(word: "Strawberry", emoji: "🍓")
-        trie.insert(word: "Avocado", emoji: "🥑")
-        trie.insert(word: "Cherry", emoji: "🍎")
-        trie.insert(word: "Eggplant", emoji: "🍆")
-        trie.insert(word: "Broccoli", emoji: "🥦")
-        trie.insert(word: "Onion", emoji: "🧅")
-        trie.insert(word: "Potato", emoji: "🥔")
-        trie.insert(word: "Carrot", emoji: "🥕")
-        trie.insert(word: "Blueberry", emoji: "🫐")
+//        trie.insert(word: "Banana", emoji: "🍌")
+//        trie.insert(word: "Watermelon", emoji: "🍉")
+//        trie.insert(word: "Mango", emoji: "🥭")
+//        trie.insert(word: "Strawberry", emoji: "🍓")
+//        trie.insert(word: "Avocado", emoji: "🥑")
+//        trie.insert(word: "Cherry", emoji: "🍒")
+//        trie.insert(word: "Eggplant", emoji: "🍆")
+//        trie.insert(word: "Blueberry", emoji: "🫐")
     }
     
     var body: some View {
-        VStack {
-            TextField("Search fruit or vegetable", text: $wordToSearch)
-                .padding()
-                .background(Color(.systemGray5))
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-            
-            Spacer()
-            
-            if searchResult.0 {
-                Text(String(searchResult.1!) + String(searchResult.1!) + String(searchResult.1!))
-                    .font(.title)
-            } else {
-                if wordToSearch != "" {
-                    Text("\(wordToSearch) not found")
+        NavigationStack {
+            VStack {
+                TextField("Search fruit", text: $wordToSearch)
+                    .padding()
+                    .background(Color(.systemGray5))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Spacer()
+                
+                if searchResult.0 {
+                    Text(String(searchResult.1!) + String(searchResult.1!) + String(searchResult.1!))
+                        .font(.title)
+                } else {
+                    if wordToSearch != "" {
+                        Text("\(wordToSearch) not found")
+                    }
+                }
+                
+                Spacer()
+            }
+            .toolbar {
+                Button {
+                    showModal.toggle()
+                    wordToSearch = ""
+                } label: {
+                    Image(systemName: "plus")
                 }
             }
-            
-            Spacer()
-        }
-        .onChange(of: wordToSearch, {
-            searchResult = trie.contains(word: wordToSearch)
-        })
+            .onChange(of: wordToSearch, {
+                searchResult = trie.contains(word: wordToSearch)
+            })
+            .sheet(isPresented: $showModal) {
+                NavigationStack {
+                    VStack {
+                        TextField("Name", text: $wordToAdd)
+                            .padding()
+                            .background(Color(.systemGray5))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        TextField("Emoji", text: $emojiToAdd)
+                            .padding()
+                            .background(Color(.systemGray5))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        Spacer()
+                    }
+                    .navigationTitle("Add Fruit")
+                    .toolbar {
+                        Button {
+                            showModal.toggle()
+                            trie.insert(word: wordToAdd, emoji: emojiToAdd)
+                            wordToAdd = ""
+                            emojiToAdd = ""
+                        } label: {
+                            Text("Done")
+                                .bold()
+                        }
+                        .disabled(wordToAdd.isEmpty || emojiToAdd.isEmpty)
+                    }
+                    .padding()
+                }
+            }
         .padding()
+        }
     }
 }
 
